c6097d8e94b1e828e9029d7f559ff7113302495e
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-04-04 05:16:39 +0000

    Update: Removed unnecessary blocks

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class PracticeProblem{
 public static void main(String args[]) {
- System.out.println(perms(""));
+ System.out.println(perms("ELO"));
 
 	}
 
@@ -13,11 +13,12 @@ public static void main(String args[]) {
 		return perm;
 	}
 
-public static void permMaker(String used, String unused, ArrayList<String> list){
+public static void permHelper(String used, String unused, ArrayList<String> list){
 if (unused.length() == 0){
 	list.add(used);
 	return;
 }
+
 String temp = unused;
 String tempused = used;
 for (int i = 0; i<unused.length(); i++){
@@ -28,27 +29,9 @@ for (int i = 0; i<unused.length(); i++){
 	else{
 		temp = unused.substring(0, i);
 			}
-	permMaker(tempused, temp, list);
+	permHelper(tempused, temp, list);
 
 }
-}
-		
-public static void permHelper(String used, String unused, ArrayList<String> list){
-	for (int i = 0; i<unused.length(); i++){
-
-String temp = unused;
-String tempused = used;
-		tempused += unused.charAt(i);
-		if (unused.length()-1>i){
-			temp = unused.substring(0, i) + unused.substring(i+1);
-				}
-		else{
-			temp = unused.substring(0, i);
-				}
-		permMaker(tempused, temp, list);
-	
-	}
-	
 }
 
 
@@ -64,7 +47,7 @@ permHelper1("", str, perm);
 	return perm;
 }
 
-public static void permMaker1(String used, String unused, ArrayList<String> list){
+public static void permHelper1(String used, String unused, ArrayList<String> list){
 if (unused.length() == 0){
 	if (!list.contains(used)){
 		list.add(used);
@@ -81,26 +64,8 @@ if (unused.length()-1>i){
 else{
 	temp = unused.substring(0, i);
 		}
-permMaker1(tempused, temp, list);
+permHelper1(tempused, temp, list);
 
 }
-}
-	
-public static void permHelper1(String used, String unused, ArrayList<String> list){
-for (int i = 0; i<unused.length(); i++){
-
-String temp = unused;
-String tempused = used;
-	tempused += unused.charAt(i);
-	if (unused.length()-1>i){
-		temp = unused.substring(0, i) + unused.substring(i+1);
-			}
-	else{
-		temp = unused.substring(0, i);
-			}
-	permMaker1(tempused, temp, list);
-
-}
-
 }
 }
